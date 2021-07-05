653dd1169f0f9d4e0c702c34089f210af989fbc3
sadowskimarcin <info@sadowskimarcin.pl>
2021-07-05 19:48:46 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { HeroFacade } from '../hero/hero.facade';
+import { HeroModel } from '../hero/models/hero.model';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private heroFacade: HeroFacade) {}
+
+  public add(): void {
+    this.heroFacade.addHero(new HeroModel('Vladimir'));
+  }
+}
