dc4304f507de2a2e8e830f937dc4fcab25ea1064
sadowskimarcin <info@sadowskimarcin.pl>
2021-07-06 09:53:13 +0200

    1 file modified

--- src/hero/hero.service.ts
+++ src/hero/hero.service.ts
@@ -5,7 +5,8 @@ import { HeroModel } from './models/hero.model';
 
 @Injectable()
 export class HeroService {
-  private apiUrl = 'https://60e2b8149103bd0017b474cc.mockapi.io/';
+  // private apiUrl = 'https://60e2b8149103bd0017b474cc.mockapi.io/';
+  private apiUrl = 'https://sadowskimarcin.free.beeceptor.com/';
   public heroes: HeroModel[] = [];
 
   constructor(private http: HttpClient) {}
