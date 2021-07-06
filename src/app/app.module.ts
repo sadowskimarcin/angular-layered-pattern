eabe7b7a1060ab8c8e4d13b0455fb59af26d4674
sadowskimarcin <info@sadowskimarcin.pl>
2021-07-06 08:39:05 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
@@ -6,7 +7,19 @@ import { AppComponent } from './app.component';
 import { HeroModule } from '../hero/hero.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HeroModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HeroModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyBpBWxIIj3CtqQlHXwaJSWXwUB28Z0iagc",
+      authDomain: "fir-example-e148c.firebaseapp.com",
+      projectId: "fir-example-e148c",
+      storageBucket: "fir-example-e148c.appspot.com",
+      messagingSenderId: "966493007774",
+      appId: "1:966493007774:web:61f4b4a53365c11d7cb08f"
+    }),
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
